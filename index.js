b319fdc9e58649104d993c50637ff5d974135fb6
itsvek03 <vivek@nimapinfotech.com>
2021-09-22 11:21:16 +0530

    Changes in the server

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const port = process.env.PORT || 5000;
 const db = require('./models')
 const http = require('http')
 const cors = require('cors')
+const path =require('path')
+
+
+
 app.use(express.json())
+
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 
 const indexrouter = require('./routes/index')
